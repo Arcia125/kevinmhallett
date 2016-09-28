b331cd2bd73685e5596dd44921c783edb14ee9bf
Arcia125 <tauren125@gmail.com>
2016-09-28 17:26:37 -0500

    remove page-text component and move projects list

--- src/app/portfolio.js
+++ src/app/portfolio.js
@@ -1,21 +1,66 @@
-var React = require('react');
-var $ = require('jquery');
-var typed = require('typed.js');
+const React = require('react');
+const $ = require('jquery');
+const typed = require('typed.js');
 
 import ImageHeader from './components/image-header/image-header';
 import Section from './components/section/section';
 import Title from './components/title/title';
-import PageText from './components/page-text/page-text';
 import Grid from './components/grid/grid';
 import Project from './components/project/project';
 
-var headerImage = require('./../images/header.png');
-var leaderboardImage = require('./../images/project-leaderboard.png');
-var recipeBoxImage = require('./../images/project-recipe-box.png');
-var simonImage = require('./../images/project-simon.png');
-var tictactoeImage = require('./../images/project-tictactoe.png');
-var calculatorImage = require('./../images/project-calculator.png');
-var pomodoroImage = require('./../images/project-pomodoro-clock.png');
+const headerImage = require('./../images/header.png');
+const leaderboardImage = require('./../images/project-leaderboard.png');
+const recipeBoxImage = require('./../images/project-recipe-box.png');
+const simonImage = require('./../images/project-simon.png');
+const tictactoeImage = require('./../images/project-tictactoe.png');
+const calculatorImage = require('./../images/project-calculator.png');
+const pomodoroImage = require('./../images/project-pomodoro-clock.png');
+
+const projects = [
+{
+	name: 'Leaderboard',
+	desc: 'Leaderboard of the top 100 students on www.freecodecamp.com using React.',
+	href: 'https://arcia125.github.io/freecodecamp-leaderboard/dist/',
+	img: leaderboardImage,
+	id: 1,
+},
+{
+	name: 'Recipe Box App',
+	desc: 'Recipe box web application using React.',
+	href: 'https://arcia125.github.io/recipe-box/dist/',
+	img: recipeBoxImage,
+	id: 2
+},
+{
+	name: 'Simon',
+	desc: 'Simon game built with HTML5 Canvas.',
+	href: 'https://arcia125.github.io/simon/dist/',
+	img: simonImage,
+	id: 3
+},
+{
+	name: 'TicTacToe',
+	desc: 'TicTacToe game using jQuery and Bootstrap.',
+	href: 'https://arcia125.github.io/tictactoe/dist/',
+	img: tictactoeImage,
+	id: 4
+},
+{
+	name: 'Calculator',
+	desc: 'Javascript Calculator built using jQuery and Bootstrap',
+	href: 'https://arcia125.github.io/calculator/dist/',
+	img: calculatorImage,
+	id: 5
+},
+{
+	name: 'Pomodoro Clock',
+	desc: 'Timer built using jQuery and jQuery-ui.',
+	href: 'https://arcia125.github.io/pomodoro-clock/dist/',
+	img: pomodoroImage,
+	id: 6
+}
+];
+
 
 
 class Portfolio extends React.Component {
@@ -36,50 +81,6 @@ class Portfolio extends React.Component {
 		}, 500);
 	}
 	render() {
-		let projects = [
-		{
-			name: 'Leaderboard',
-			desc: 'Leaderboard of the top 100 students on www.freecodecamp.com using React.',
-			href: 'https://arcia125.github.io/freecodecamp-leaderboard/dist/',
-			img: leaderboardImage,
-			id: 1,
-		},
-		{
-			name: 'Recipe Box App',
-			desc: 'Recipe box web application using React.',
-			href: 'https://arcia125.github.io/recipe-box/dist/',
-			img: recipeBoxImage,
-			id: 2
-		},
-		{
-			name: 'Simon',
-			desc: 'Simon game built with HTML5 Canvas.',
-			href: 'https://arcia125.github.io/simon/dist/',
-			img: simonImage,
-			id: 3
-		},
-		{
-			name: 'TicTacToe',
-			desc: 'TicTacToe game using jQuery and Bootstrap.',
-			href: 'https://arcia125.github.io/tictactoe/dist/',
-			img: tictactoeImage,
-			id: 4
-		},
-		{
-			name: 'Calculator',
-			desc: 'Javascript Calculator built using jQuery and Bootstrap',
-			href: 'https://arcia125.github.io/calculator/dist/',
-			img: calculatorImage,
-			id: 5
-		},
-		{
-			name: 'Pomodoro Clock',
-			desc: 'Timer built using jQuery and jQuery-ui.',
-			href: 'https://arcia125.github.io/pomodoro-clock/dist/',
-			img: pomodoroImage,
-			id: 6
-		}
-		];
 		return (
 			<div className='app' >
 				<ImageHeader imgSrc={headerImage} className='splash-header' >
@@ -96,12 +97,12 @@ class Portfolio extends React.Component {
 
 				<Section id='about-me' className='about-me-section'>
 					<Title className='about-me-title' >About Me</Title>
-					<PageText className='about-me-paragraph' >
+					<p className='about-me-paragraph' >
 						I am looking for work as a front end developer here in Austin, Texas. As a web developer, I am constantly looking
 						to improve my knowledge about software engineering and the latest web
 						technologies. I would like to get involved in projects where I can
 						develop my skills as a programmer and make a meaningful contribution to a team.
-					</PageText>
+					</p>
 				</Section>
 
 				<Section className='portfolio-section' >
